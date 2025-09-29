59349e86ea665f57ef95ba66d7a23796775aaac9
Timm Le <letttrivn.go0g1e@gmail.com>
2025-09-29 13:57:31 -0400

    AoJ update 5.82

--- scripts/Ally of Justice Flamvell/c21417154.lua
+++ scripts/Ally of Justice Flamvell/c21417154.lua
@@ -85,7 +85,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
 	--Cannot Special Summon from the Extra Deck, except Ally Monsters
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,4))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
