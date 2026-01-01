8b82ed844e64805a54d982fb326491a581dd568f
Sif <sifthegreatgreywolf.ds@gmail.com>
2026-01-01 09:26:25 -0500

    AoJ/F/G Update 1.4

--- scripts/Ally of Justice Flamvell Genex/c38062333.lua
+++ scripts/Ally of Justice Flamvell Genex/c38062333.lua
@@ -18,13 +18,13 @@ function s.initial_effect(c)
 	e1:SetTarget(s.tdtg)
 	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)]]--
-	--To Deck top
+	--To Deck top/Fix topdeck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetRange(LOCATION_SZONE)
-	e1:SetCountLimit(1)
-	e1:SetTarget(s.dttg)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	--e1:SetTarget(s.dttg)
 	e1:SetOperation(s.dtop)
 	c:RegisterEffect(e1)
 	--Extender
@@ -140,7 +140,7 @@ function s.statval(e,c,re,chk)
 	return SET_ALLY_OF_JUSTICE and SET_FLAMVELL and SET_GENEX and RACE_PYRO and ATTRIBUTE_FIRE
 end
 
---Decktop
+--[[Decktop
 function s.dttfilter(c)
 	return (c:IsSetCard(s.listed_series))
 end
@@ -155,8 +155,19 @@ function s.dtop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.MoveSequence(g:GetFirst(),0)
 		Duel.ConfirmDecktop(tp,1)
 	end
-end
+end]]--
 
+--Fix topdeck
+function s.dtop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetDecktopGroup(tp,1)
+	if #g==0 then return end
+	Duel.ConfirmCards(tp,g)
+	local tc=g:GetFirst()
+	local opt=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
+	if opt==1 then
+		Duel.MoveSequence(tc,opt)
+	end
+end
 --To Deck
 --[[function s.tdconfilter(c)
 	return c:IsFaceup() and c:IsSetCard(SET_FLAMVELL) and (c:IsAttackAbove(1800) or c:IsDefenseAbove(1800))
