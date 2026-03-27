cd253e0b74291e336f656b3ff396a465606e62c4
ItsNotTimmLol <letttrivn.go0g1e@gmail.com>
2026-03-26 22:35:28 -0400

    AoJ/F/G Update 2.01 - CCC updates

--- scripts/Ally of Justice Flamvell Genex/c21417154.lua
+++ scripts/Ally of Justice Flamvell Genex/c21417154.lua
@@ -1,205 +1,106 @@
---Ally of Justice Field
+--Ally of Justice Release Reverse
 --Scripted by WolfSif
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetDescription(aux.Stringid(id,0))
 	e0:SetCode(EVENT_FREE_CHAIN)
-	--e0:SetTarget(s.acttg)
-	--e0:SetOperation(s.actop)
-	--e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e0:SetCost(s.actcost)
 	c:RegisterEffect(e0)
 	--Can be activated from the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCondition(function(e) return Duel.IsExistingMatchingCard(s.actcostfilter,e:GetHandlerPlayer(),LOCATION_HAND,0,1,nil) end)
-	e1:SetValue(function(e,c) e:SetLabel(1) end)
+	e1:SetCondition(function(e) return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)==0 and Duel.IsBattlePhase() end)
 	e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
 	c:RegisterEffect(e1)
-	e0:SetLabelObject(e1)
-	--Monsters with different traits are unaffected by your opponent's activated effects
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_IMMUNE_EFFECT)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(s.immtg)
-	e2:SetValue(s.immval)
-	c:RegisterEffect(e2)
-	--Add to hand
+	--extra Tribute material
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e5:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetTargetRange(0,LOCATION_MZONE)
+	e5:SetValue(POS_FACEUP|POS_FACEDOWN)
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp) end)
-	e3:SetTarget(s.thtg)
-	e3:SetOperation(s.thop)
+	e3:SetTargetRange(LOCATION_HAND,0)
+	e3:SetTarget(function(e,c) return c:IsMonster() and c:IsAttribute(ATTRIBUTE_DARK) and (c:IsCode(s.ally_names) or c:IsSetCard(SET_ALLY_OF_JUSTICE) or c:IsSetCard(SET_GENEX_ALLY)) end)
+	e3:SetLabelObject(e5)
 	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_SUMMON_COST)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
+	e4:SetTarget(s.sumtg)
+	e4:SetCost(s.ccost)
+	e4:SetOperation(s.acop)
 	c:RegisterEffect(e4)
-	--Change to LIGHT or face-down
-	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,3))
-	e6:SetCategory(CATEGORY_POSITION)
-	e6:SetType(EFFECT_TYPE_QUICK_O)
-	e6:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e6:SetCode(EVENT_FREE_CHAIN)
-	e6:SetRange(LOCATION_SZONE)
-	e6:SetCountLimit(1)
-	e6:SetTarget(s.nstg)
-	e6:SetOperation(s.nsop)
-	c:RegisterEffect(e6)
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_SUMMON)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
 end
-s.listed_series={SET_ALLY_OF_JUSTICE,SET_FLAMVELL,SET_GENEX}
-s.ally_series={SET_ALLY_OF_JUSTICE,SET_FLAMVELL,SET_GENEX}
+s.listed_series={SET_ALLY_OF_JUSTICE,SET_WORM}
+s.ally_series={SET_ALLY_OF_JUSTICE}
 s.ally_names={40155554,59482302}
-
---activate from hand
-function s.actcostfilter(c)
-	return c:IsDiscardable() 
-		--and (c:IsCode(s.ally_names) or c:IsSetCard(s.ally_series)) 
-		--and c:IsMonster() 
+function s.sumtg(e,c,tp)
+	local tp=e:GetHandlerPlayer()
+	local ct=c:GetMaterial():Filter(Card.IsPreviousControler,nil,1-tp):GetSum(Card.GetBaseAttack)
+	if ct>0 then e:SetLabel(ct) end
+	Debug.Message("Labeltg: "..(ct))
+	return ct>0
 end
-function s.actcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.actcostfilter,tp,LOCATION_HAND,0,1,c)
+function s.ccost(e,c,tp)
+	local ct=e:GetLabel()>=0
+	Debug.Message("Labelcost: "..(ct))
+	return Duel.CheckLPCost(tp,ct)
 end
-
---cost
-function s.actcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local label_obj=e:GetLabelObject()
-	if chk==0 then label_obj:SetLabel(0) return true end
-	if label_obj:GetLabel()>0 then
-		label_obj:SetLabel(0)
-		Duel.DiscardHand(tp,s.actcostfilter,1,1,REASON_COST|REASON_DISCARD)
-	end
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()>=0
+	Debug.Message("Labelop: "..(ct))
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.PayLPCost(tp,ct)
 end
-
---place spells
-function s.actfilter(c,tp)
-	return (c:IsType(TYPE_FIELD) or c:IsType(TYPE_CONTINUOUS))
-		and c:IsSetCard(s.ally_series) 
-		and c:IsSpell() and not c:IsForbidden() and c:CheckUniqueOnField(tp)
-end
-function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return (Duel.GetLocationCount(tp,LOCATION_SZONE)>0 or Duel.GetLocationCount(tp,LOCATION_FZONE)>0)
-		and Duel.IsExistingMatchingCard(s.actfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,tp) end
-end
-function s.actop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local g=Duel.GetMatchingGroup(s.actfilter,tp,LOCATION_HAND|LOCATION_DECK,0,nil,tp) 
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-		if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 and Duel.GetLocationCount(tp,LOCATION_FZONE)<=0 then return end
-		local sc=Duel.SelectMatchingCard(tp,s.actfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,tp):GetFirst()
-		local loc=LOCATION_SZONE
-		if sc:IsType(TYPE_FIELD) then loc=LOCATION_FZONE end
-		if sc then
-			Duel.MoveToField(sc,tp,tp,loc,POS_FACEUP,true)
-		end
-	end
-end
-
---immune
-function s.immtg(e,c)
-	return (c:IsCode(s.ally_names) or c:IsSetCard(s.ally_series)) 
-		and c:IsMonster() 
-		and (c:IsAttributeExcept(c:GetOriginalAttribute())
-			or not (c:IsCode(c:GetOriginalCode())
-			or c:IsAttack(c:GetBaseAttack())
-			or c:IsDefense(c:GetBaseDefense())))
-end
-function s.immval(e,te)
-	return te:GetOwnerPlayer()==1-e:GetHandlerPlayer() and te:IsActivated()
-end
-
---Add on opp monster
-function s.thfilter(c)
-	return (c:IsSetCard(s.listed_series) or c:IsCode(s.ally_names))
-		and c:IsAbleToHand()
-		and not c:IsCode(id)
+function s.spfilter(c,e,tp)
+	return (c:IsCode(s.ally_names) or c:IsSetCard(s.listed_series)) 
+		and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp)
 		and c:IsMonster()
-		--and not Duel.IsExistingMatchingCard(s.uniquefilter,tp,LOCATION_MZONE|LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil,c:GetCode())
-end
-function s.uniquefilter(c,code)
-	return c:IsCode(code) and c:IsFaceup() and c:IsMonster()
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-		end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.nsfilter(c)
-	return (c:IsSetCard(s.listed_series) or c:IsCode(s.ally_names))
+	return (c:IsCode(s.ally_names) or c:IsSetCard(s.listed_series)) 
+		and c:IsMonster()
 		and c:IsSummonable(true,nil)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
-	if tc and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) then
-		Duel.ConfirmCards(1-tp,tc)
-		Duel.ShuffleHand(tp)
-		--Normal Summon
-		--[[if Duel.IsExistingMatchingCard(s.nsfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,nil)
-			and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-			local sc=Duel.SelectMatchingCard(tp,s.nsfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,nil):GetFirst()
-			if sc then
-				Duel.BreakEffect()
-				Duel.SummonOrSet(tp,sc,true,nil)
-			end
-		end]]--
-	end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,nil,e,tp)
+	local g2=Duel.GetMatchingGroup(s.nsfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil,e,tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return #g1>0 and #ft>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,1-tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-
-
---Change Attribute or to facedown
-function s.posfilter(c)
-	return c:IsFaceup() and (s.pos1filter(c) or s.pos2filter(c))
-end
-function s.pos1filter(c)
-	return not c:IsAttribute(ATTRIBUTE_LIGHT)
-end
-function s.pos2filter(c)
-	return (c:IsAttribute(ATTRIBUTE_LIGHT) or c:GetAttribute()==ATTRIBUTE_LIGHT) and c:IsCanTurnSet()
-end
-function s.nstg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	if chk==0 then return Duel.IsExistingTarget(s.posfilter,tp,LOCATION_MZONE,0,1,nil) or Duel.IsExistingTarget(s.posfilter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g=Duel.SelectTarget(tp,s.posfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
-end
-function s.nsop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g1=Duel.GetTargetCards(e):Filter(s.pos1filter,nil)
-	local g2=Duel.GetTargetCards(e):Filter(s.pos2filter,nil)
+function s.spop(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g1=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e,tp)
 	if #g1>0 then
-		local tc1=g1:GetFirst()
-		for tc1 in aux.Next(g1) do
-			--It becomes LIGHT
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetCode(EFFECT_ADD_ATTRIBUTE)
-			e1:SetValue(ATTRIBUTE_LIGHT)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-			tc1:RegisterEffect(e1)
-		end
-	elseif #g2>0 then
-		local tc2=g2:GetFirst()
-		for tc2 in aux.Next(g2) do
-			Duel.ChangePosition(tc2,POS_FACEDOWN_DEFENSE)
-		end
+		Duel.SpecialSummon(g1,0,tp,1-tp,false,false,POS_FACEUP)
+	end
+	local g2=Duel.GetMatchingGroup(s.nsfilter,tp,LOCATION_HAND,0,nil,true,nil)
+	if #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+		Duel.BreakEffect()
+		Duel.ShuffleHand(tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+		sg=g2:Select(tp,1,1,nil):GetFirst()
+		Duel.Summon(tp,sg,true,nil)
 	end
 end
-
--- scripts/Ally of Justice Flamvell Genex/c43773304.lua
+++ scripts/Ally of Justice Flamvell Genex/c43773304.lua
@@ -11,17 +11,67 @@ function s.initial_effect(c)
 	e1:SetTarget(s.settg)
 	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
+	--Special Summon this card from your hand or GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
+	e2:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--switch atk/def
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,4))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_CHANGE_POS)
+	e3:SetCondition(s.adcon)
+	e3:SetTarget(s.adtg)
+	e3:SetOperation(s.adop)
+	c:RegisterEffect(e3)
+	--LIGHT during Battle Phase and board
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_GRAVE,LOCATION_ONFIELD|LOCATION_GRAVE)
+	e4:SetCondition(s.lightcon1)
+	e4:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e4:SetValue(ATTRIBUTE_LIGHT)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCondition(s.lightcon2)
+	e5:SetTargetRange(LOCATION_ONFIELD|LOCATION_GRAVE,LOCATION_GRAVE)
+	c:RegisterEffect(e5)
+	local e6=e4:Clone()
+	e6:SetTargetRange(LOCATION_GRAVE,LOCATION_GRAVE)
+	e6:SetTarget(s.changegytg)
+	c:RegisterEffect(e6)
+	--Code check LIGHT
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_FIELD)
+	e7:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e7:SetCode(id)
+	e7:SetRange(LOCATION_SZONE)
+	e7:SetTargetRange(0,1)
+	e7:SetValue(s.attval)
+	c:RegisterEffect(e7)
 end
-function s.filter(c)
+s.listed_series={SET_ALLY_OF_JUSTICE,SET_FLAMVELL}
+s.ally_series={SET_ALLY_OF_JUSTICE,SET_GENEX_ALLY}
+s.ally_names={40155554,59482302}
+--forced Set
+function s.setfilter(c)
 	return c:IsSSetable() or c:IsMSetable(true,nil)
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) or Duel.IsExistingMatchingCard(s.filter,1-tp,0,LOCATION_HAND,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND,0,1,nil) or Duel.IsExistingMatchingCard(s.setfilter,1-tp,0,LOCATION_HAND,1,nil) end
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g1=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
 	local g2=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
-	local sc1=g1:FilterSelect(tp,s.filter,1,1,nil):GetFirst()
+	local sc1=g1:FilterSelect(tp,s.setfilter,1,1,nil):GetFirst()
 	if sc1:IsMSetable(true,nil) then
 		Duel.MSet(tp,sc1,true,nil)
 	else
@@ -32,7 +82,7 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD_EXC_GRAVE|RESET_PHASE|PHASE_END)
 		sc1:RegisterEffect(e1)
 	end
-	local sc2=g2:FilterSelect(1-tp,s.filter,1,1,nil):GetFirst()
+	local sc2=g2:FilterSelect(1-tp,s.setfilter,1,1,nil):GetFirst()
 	if sc2:IsMSetable(true,nil) then
 		Duel.MSet(1-tp,sc2,true,nil)
 	else
@@ -43,4 +93,85 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetReset(RESET_EVENT|RESETS_STANDARD_EXC_GRAVE|RESET_PHASE|PHASE_END)
 		sc2:RegisterEffect(e2)
 	end
+end
+
+--Special Summon
+function s.thfilter(c,tp)
+	return (c:IsCode(s.ally_names) 
+	or c:IsSetCard(s.ally_series) 
+	or (c:IsSetCard(SET_WORM) and c:IsRace(RACE_REPTILE))) 
+	and c:IsMonster() and c:IsAbleToHand()
+	and not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,c:GetCode()),tp,LOCATION_ONFIELD|LOCATION_GRAVE,LOCATION_ONFIELD,1,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE))
+		or (Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP+POS_FACEDOWN_DEFENSE,1-tp))
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+	local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP+POS_FACEDOWN_DEFENSE,1-tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	if not (b1 or b2) then return end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,2)},
+		{b2,aux.Stringid(id,3)})
+	local target_player=op==1 and tp or 1-tp
+	local target_pos=POS_FACEDOWN_DEFENSE
+	if target_player==1-tp then target_pos=target_pos+POS_FACEUP end
+	Duel.SpecialSummon(c,0,tp,target_player,false,false,target_pos)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+	if #g>0 then
+		Duel.BreakEffect()
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+--swap stats
+function s.adcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousPosition(POS_FACEUP)
+end
+
+function s.adtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+end
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SWAP_BASE_AD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e1)
+	end
+end
+
+--LIGHT stats
+function s.lightcon1(e,tp,eg,ep,ev,re,r,rp) 
+	return Duel.IsBattlePhase() and e:GetHandler():GetControler()==e:GetHandler():GetOwner()
+end
+function s.lightcon2(e,tp,eg,ep,ev,re,r,rp) 
+	return Duel.IsBattlePhase() and e:GetHandler():GetControler()~=e:GetHandler():GetOwner()
+end
+function s.changegytg(e,c)
+	if c:GetFlagEffect(1)==0 then
+		c:RegisterFlagEffect(1,0,0,0)
+		local eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+		c:ResetFlagEffect(1)
+		for _,te in ipairs(eff) do
+			local op=te:GetOperation()
+			if not op or op(e,c) then return false end
+		end
+	end
+	return c:IsMonster()
+end
+function s.attval(e,c,re,chk)
+	if chk==0 then return true end
+	return ATTRIBUTE_LIGHT
 end
