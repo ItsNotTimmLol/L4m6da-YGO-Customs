c24036bdb140ff41122d924601bf3dfcc1c7ca05
ItsNotTimmLol <letttrivn.go0g1e@gmail.com>
2026-07-13 22:33:33 -0400

    Worm Update

--- scripts/Ally of Justice Flamvell Genex/c40079081.lua
+++ scripts/Ally of Justice Flamvell Genex/c40079081.lua
@@ -1,4 +1,4 @@
---Ally of Justice Release Reverse
+--W Nebula Terrorspace
 --Scripted by WolfSif
 local s,id=GetID()
 function s.initial_effect(c)
@@ -13,51 +13,82 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
-	e1:SetCondition(function(e)
-		return Duel.GetTurnCount()==1 and Duel.IsStandbyPhase(1-e:GetHandlerPlayer())
-	end)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 	c:RegisterEffect(e2)
+	--atkchange
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_SUMMON)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
-	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_MAIN_END)
-	e3:SetCondition(function(_,tp) return Duel.IsTurnPlayer(1-tp) and Duel.IsMainPhase() end)
-	e3:SetTarget(s.nstg)
-	e3:SetOperation(s.nsop)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetCode(EFFECT_SET_BASE_ATTACK)
+	e3:SetTarget(s.atkdeftg)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
-	--Flip face-up
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,3))
-	e4:SetCategory(CATEGORY_POSITION)
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_MAIN_END)
-	e4:SetCountLimit(1)
-	e4:SetTarget(s.postg)
-	e4:SetOperation(s.posop)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_SET_BASE_DEFENSE)
+	e4:SetValue(s.defval)
 	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_SUMMON)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
+	e5:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_MAIN_END)
+	e5:SetTarget(s.nstg)
+	e5:SetOperation(s.nsop)
+	c:RegisterEffect(e5)
+	--Flip face-up
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetCategory(CATEGORY_POSITION)
+	e6:SetType(EFFECT_TYPE_QUICK_O)
+	e6:SetRange(LOCATION_SZONE)
+	e6:SetCode(EVENT_FREE_CHAIN)
+	e6:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_MAIN_END)
+	e6:SetCountLimit(1)
+	e6:SetTarget(s.postg)
+	e6:SetOperation(s.posop)
+	c:RegisterEffect(e6)
 end
+s.listed_names={88438982}
 s.listed_series={SET_ALLY_OF_JUSTICE,SET_WORM}
 s.ally_series={SET_ALLY_OF_JUSTICE}
 s.ally_names={40155554,59482302}
+function s.atkconfilter(c)
+	return c:IsFaceup() and c:IsCode(88438982) and c:IsSummonType(SUMMON_TYPE_FLIP)
+end
+function s.atkdeftg(e,c)
+	return not c:IsSummonType(SUMMON_TYPE_FLIP)
+end
+function s.atkval(e,c)
+	local ct=Duel.GetMatchingGroupCount(s.atkconfilter,0,LOCATION_MZONE,0,nil)
+	local dct=Duel.GetMatchingGroupCount(s.atkdeftg,0,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if ct<1 then ct=0.5 end
+	Debug.Message("ct: "&ct)
+	Debug.Message("dct: "&dct)
+	return c:GetBaseAttack()/(ct*2)
+end
+function s.defval(e,c)
+	local ct=Duel.GetMatchingGroupCount(s.atkconfilter,0,LOCATION_MZONE,0,nil)
+	if ct<1 then ct=0.5 end
+	return c:GetBaseDefense()/(ct*2)
+end
+
 function s.nsfilter(c)
 	return c:IsRace(RACE_REPTILE) and c:IsSetCard(SET_WORM)
 		and c:IsMonster()
 		and c:IsSummonable(true,nil)
 end
 function s.nstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.nsfilter,tp,LOCATION_HAND,0,nil,true,nil)
 	local ft=Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)
-	if chk==0 then return ft>0 end
+	if chk==0 then return ft>0 and #g>0 end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.nsop(e,tp,eg,ep,ev,re,r,rp,chk)
