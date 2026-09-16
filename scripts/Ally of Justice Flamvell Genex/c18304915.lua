312d5537be60dab390a858c410ddf58375cd6f1d
ItsNotTimmLol <letttrivn.go0g1e@gmail.com>
2026-09-16 11:46:39 -0400

    Worm 2.0

--- scripts/Ally of Justice Flamvell Genex/c18304915.lua
+++ scripts/Ally of Justice Flamvell Genex/c18304915.lua
@@ -6,9 +6,6 @@ function s.initial_effect(c)
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
-	--e0:SetDescription(aux.Stringid(id,0))
-	--e0:SetTarget(s.thtg)
-	--e0:SetOperation(s.actop)
 	c:RegisterEffect(e0)
 	--Negate
 	local e1=Effect.CreateEffect(c)
@@ -32,31 +29,20 @@ function s.initial_effect(c)
 	e3:SetCode(EFFECT_MUST_ATTACK)
 	e3:SetTargetRange(0,LOCATION_MZONE)
 	c:RegisterEffect(e3)
-	--[[Return "Worm Call"
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY)
-	e4:SetCode(EVENT_CHAIN_SOLVING)
-	e4:SetRange(LOCATION_FZONE)
-	e4:SetCondition(s.regcon)
-	e4:SetOperation(s.regop)
-	c:RegisterEffect(e4)]]--
-	--Give control
+	--Chain
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,0))
 	e5:SetCategory(CATEGORY_CONTROL+CATEGORY_DRAW+CATEGORY_TOHAND)
 	e5:SetType(EFFECT_TYPE_QUICK_O)
 	e5:SetCode(EVENT_CHAINING)
 	e5:SetRange(LOCATION_FZONE)
-	e5:SetCondition(s.ctrlcon)
-	e5:SetTarget(s.ctrltg)
-	e5:SetOperation(s.ctrlop)
+	e5:SetCondition(s.chaincon)
+	e5:SetOperation(s.chainop)
 	e5:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
 	c:RegisterEffect(e5)
 	--Set 1 "Dimensionhole" and/or "Worm Call"
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,2))
+	e6:SetDescription(aux.Stringid(id,1))
 	e6:SetCategory(CATEGORY_SET)
 	e6:SetType(EFFECT_TYPE_IGNITION)
 	e6:SetRange(LOCATION_FZONE)
@@ -72,28 +58,35 @@ s.w_nebula_names={18304915,30476000,40079081,53842829,55939812,76108887,90075978
 function s.thfilter(c)
 	return c:IsFaceup() and c:IsSetCard(SET_WORM) and c:IsAbleToHand() and not c:IsCode(id)
 end
-function s.ctrlcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:GetHandler()~=e:GetHandler()
+function s.chaincon(e,tp,eg,ep,ev,re,r,rp)
+	local b1=Duel.GetMZoneCount(1-tp,g,tp)>0 and Duel.IsExistingMatchingCard(s.chainfilter,tp,LOCATION_MZONE,0,1,nil)
+	local b2=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	return re:GetHandler()~=e:GetHandler() and (b1 or b2)
 end
-function s.ctrlfilter(c)
+function s.chainfilter(c)
 	return (c:IsRace(RACE_REPTILE) and c:IsSetCard(SET_WORM)) and c:IsControlerCanBeChanged()
 end
-function s.ctrltg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetMZoneCount(1-tp,g,tp)>0 and Duel.IsExistingMatchingCard(s.ctrlfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_CONTROL,nil,1,tp,LOCATION_MZONE)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-end
-function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.ctrlfilter,tp,LOCATION_MZONE,0,nil)
-	if #g==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
-	local sg=g:Select(tp,1,1,nil)
-	if sg and Duel.GetControl(sg,1-tp) and Duel.IsPlayerCanDraw(tp) then
-		Duel.BreakEffect()
-		Duel.Draw(tp,#sg,REASON_EFFECT)
+function s.chainop(e,tp,eg,ep,ev,re,r,rp)
+	local b1=Duel.GetMZoneCount(1-tp,g,tp)>0 and Duel.IsExistingMatchingCard(s.chainfilter,tp,LOCATION_MZONE,0,1,nil)
+	local b2=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	if not (b1 or b2) then return end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,3)},
+		{b2,aux.Stringid(id,4)})
+	if op==1 then
+		--Give control to draw 
+		local g=Duel.GetMatchingGroup(s.chainfilter,tp,LOCATION_MZONE,0,nil)
+		if #g==0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+		local sg=g:Select(tp,1,1,nil)
+		if sg and Duel.GetControl(sg,1-tp) and Duel.IsPlayerCanDraw(tp) then
+			Duel.BreakEffect()
+			Duel.Draw(tp,#sg,REASON_EFFECT)
+		end
+	elseif op==2 then
+		--Return to hand
 		local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-		if #tc>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		if #tc>0 then
 			Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		end
 	end
--- scripts/Ally of Justice Flamvell Genex/c40079081.lua
+++ scripts/Ally of Justice Flamvell Genex/c40079081.lua
@@ -1,24 +1,21 @@
---W Nebula Terrorspace
+--W Nebula Call
 --Scripted by WolfSif
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	--e0:SetOperation(s.actop)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
 	--Can be activated from the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	--e1:SetCondition(s.actcon)
 	e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	--e2:SetCondition(s.actcon)
 	c:RegisterEffect(e2)
 	--[[atkchange
 	local e3=Effect.CreateEffect(c)
@@ -82,56 +79,11 @@ function s.initial_effect(c)
 	e9:SetTarget(s.nstg)
 	e9:SetOperation(s.nsop)
 	c:RegisterEffect(e9)
-	--[[Flip face-up
-	local e8=Effect.CreateEffect(c)
-	e8:SetDescription(aux.Stringid(id,5))
-	e8:SetCategory(CATEGORY_POSITION)
-	e8:SetType(EFFECT_TYPE_QUICK_O)
-	e8:SetRange(LOCATION_SZONE)
-	e8:SetCode(EVENT_FREE_CHAIN)
-	e8:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_MAIN_END)
-	e8:SetCountLimit(1)
-	e8:SetTarget(s.postg)
-	e8:SetOperation(s.posop)
-	c:RegisterEffect(e8)
-	aux.GlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_FLIP)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-		ge2:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge2,0)
-	end)]]--
 end
 s.roll_dice=true
 s.listed_names={88438982}
 s.listed_series={SET_WORM}
 s.w_nebula_names={18304915,30476000,40079081,53842829,55939812,76108887,90075978}
---Extra activation condition
-function s.actcon(e,tp,eg,ep,ev,re,r,rp)
-	local tp=e:GetHandler():GetControler()
-	return Duel.IsTurnPlayer(1-tp)
-end
---Activate Field Spell
-function s.actfilter(c,tp)
-	return c:IsCode(s.w_nebula_names) and c:GetActivateEffect() and c:GetActivateEffect():IsActivatable(tp,true)
-		and c:IsType(TYPE_FIELD)
-end
-function s.actop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(s.actfilter,tp,LOCATION_DECK,0,nil,tp)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-		local tc=g:Select(tp,1,1,nil):GetFirst()
-		if tc:IsType(TYPE_FIELD) then
-			Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
-		end
-	end
-end
 --Dimikles buff
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	eg:GetFirst():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
@@ -166,18 +118,17 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,2)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
-function s.thfilter(c)
+function s.thfilter(c,dc)
 	return c:IsRace(RACE_REPTILE) and c:IsSetCard(SET_WORM)
-		and c:IsMonster() and c:HasLevel() and c:IsAbleToHand()
+		and c:IsMonster() and c:HasLevel() and not c:IsLevel(dc/2) and c:IsAbleToHand() and (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED)) 
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local d1,d2=Duel.TossDice(tp,2)
+	local dn=2
+	local d1,d2=Duel.TossDice(tp,dn)
 	local dc=(d1+d2)
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_ONFIELD|LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,nil)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_ONFIELD|LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,nil,dc)
 	local sg=g:SelectWithSumEqual(tp,Card.GetLevel,dc,1,2)
-	if not (#sg>0 and #sg==sg:GetClassCount(Card.GetLevel)) then
-		sg=g:SelectWithSumEqual(tp,Card.GetLevel,dc,1,2)
-	else
+	if #sg>0 then
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
 	end
