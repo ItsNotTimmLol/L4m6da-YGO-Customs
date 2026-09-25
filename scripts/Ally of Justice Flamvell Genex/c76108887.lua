904610c6e7766768565fa41d4552e28adda00a3c
ItsNotTimmLol <letttrivn.go0g1e@gmail.com>
2026-09-24 22:09:34 -0400

    Worm 2.0

--- scripts/Ally of Justice Flamvell Genex/c76108887.lua
+++ scripts/Ally of Justice Flamvell Genex/c76108887.lua
@@ -7,7 +7,6 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
 	e1:SetCondition(s.handcon)
 	c:RegisterEffect(e1)
-
 	--Negate activation
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_REMOVE)
@@ -17,7 +16,6 @@ function s.initial_effect(c)
 	e2:SetTarget(s.negtg)
 	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
-
 	--Place 1 Worm Counter from GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
@@ -43,29 +41,21 @@ function s.handcon(e)
 	local tp=e:GetHandlerPlayer()
 	return Duel.IsExistingMatchingCard(s.dimiklesfilter,tp,LOCATION_MZONE,0,1,nil)
 end
---==================================================
--- Negation effect
---==================================================
+--Negate
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp
 		and re:IsActiveType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP)
 end
-
 function s.countercostfilter(c)
 	return c:IsFaceup() and c:GetCounter(COUNTER_WORM)>0
 end
-
 function s.remfilter(c)
 	return c:IsFaceup() and c:IsControler(tp)
 end
-
---At least one of the two effects must be possible
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-
 	local can_counter=false
 	local can_position=false
-
 	--Effect 1: Remove 3 Worm Counters
 	local total=0
 	local g=Duel.GetMatchingGroup(s.countercostfilter,tp,LOCATION_ONFIELD,0,nil)
@@ -73,24 +63,17 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 		total=total+tc:GetCounter(COUNTER_WORM)
 	end
 	can_counter=(total>=3)
-
 	--Effect 2: Change monsters you own, including Worm Dimikles
-	local dim=Duel.IsExistingMatchingCard(
-		function(tc)
+	local dim=Duel.IsExistingMatchingCard(function(tc)
 			return tc:IsCode(88438982)
 				and tc:IsControler(tp)
 				and tc:IsLocation(LOCATION_MZONE)
 				and (tc:IsFaceup() or tc:IsFacedown())
-		end,
-		tp,LOCATION_MZONE,0,1,nil
-	)
-
+		end,tp,LOCATION_MZONE,0,1,nil)
 	can_position=dim
-
 	if chk==0 then
 		return can_counter or can_position
 	end
-
 	local opt
 	if can_counter and can_position then
 		opt=Duel.SelectOption(tp,
@@ -101,9 +84,7 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	else
 		opt=1
 	end
-
 	e:SetLabel(opt)
-
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
 end
@@ -117,15 +98,12 @@ end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local opt=e:GetLabel()
-
 	--Remove 3 Worm Counters
 	if opt==0 then
 		local ct=3
 		local g=Duel.GetMatchingGroup(s.countercostfilter,tp,LOCATION_ONFIELD,0,nil)
-
 		while ct>0 and #g>0 do
 			local tc
-
 			if ct==1 then
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_COUNTER)
 				tc=g:Select(tp,1,1,nil):GetFirst()
@@ -133,43 +111,28 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_COUNTER)
 				tc=g:Select(tp,1,1,nil):GetFirst()
 			end
-
 			if not tc then return end
-
 			local remove=math.min(tc:GetCounter(COUNTER_WORM),ct)
 			tc:RemoveCounter(tp,COUNTER_WORM,remove,REASON_EFFECT)
-
 			ct=ct-remove
 			g=Duel.GetMatchingGroup(s.countercostfilter,tp,LOCATION_ONFIELD,0,nil)
 		end
-
 		if ct>0 then return end
-
 		if Duel.NegateActivation(ev) then
 			Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
 		end
-
 	--Change monsters you own to Defense Position
 	else
-		local dim=Duel.GetMatchingGroup(
-			function(tc)
+		local dim=Duel.GetMatchingGroup(function(tc)
 				return tc:IsCode(88438982)
 					and tc:IsControler(tp)
 					and tc:IsLocation(LOCATION_MZONE)
-			end,
-			tp,LOCATION_MZONE,0,nil
-		)
-
+			end,tp,LOCATION_MZONE,0,nil)
 		if #dim==0 then return end
-
-		local g=Duel.GetMatchingGroup(
-			s.negotargetfilter,tp,LOCATION_MZONE,0,nil,tp
-		)
-
+		local g=Duel.GetMatchingGroup(s.negotargetfilter,tp,LOCATION_MZONE,0,nil,tp)
 		--The selected group must include Worm Dimikles.
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
 		local sg=g:Select(tp,1,#g,nil)
-
 		--If the player somehow selected a group without Dimikles,
 		--force Dimikles into the group.
 		if not sg:IsExists(function(tc)
@@ -180,18 +143,14 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 				sg:AddCard(dc)
 			end
 		end
-
 		if #sg==0 then return end
-
 		local changed=0
-
 		for tc in aux.Next(sg) do
 			if tc:IsFaceup() then
 				--Choose face-up or face-down Defense Position
 				local pos=Duel.SelectOption(tp,
 					aux.Stringid(id,4),
 					aux.Stringid(id,5))
-
 				if pos==0 then
 					if Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)~=0 then
 						changed=changed+1
@@ -206,7 +165,6 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 				changed=changed+1
 			end
 		end
-
 		if changed>0 then
 			if Duel.NegateActivation(ev) then
 				Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
@@ -215,33 +173,22 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
---==================================================
--- GY effect
---==================================================
-
+--Place counter
 function s.ctfilter(c)
 	return c:IsFaceup()
 		and c:IsControler(tp)
 		and c:IsCanAddCounter(COUNTER_WORM,1)
 end
-
 function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		return Duel.IsExistingMatchingCard(
-			s.ctfilter,tp,LOCATION_ONFIELD,0,1,nil
-		)
+		return Duel.IsExistingMatchingCard(s.ctfilter,tp,LOCATION_ONFIELD,0,1,nil)
 	end
-
 	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,tp,LOCATION_ONFIELD)
 end
-
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_COUNTER)
-	local g=Duel.SelectMatchingCard(
-		tp,s.ctfilter,tp,LOCATION_ONFIELD,0,1,1,nil
-	)
+	local g=Duel.SelectMatchingCard(tp,s.ctfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
 	local tc=g:GetFirst()
-
 	if tc then
 		tc:AddCounter(COUNTER_WORM,1)
 	end
